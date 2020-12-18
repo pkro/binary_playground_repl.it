0741af10d7e4ca3c33a0384bf636738a5b60c431
pkro <pkrombach@web.de>
2020-12-18 09:27:34 +0000

    comment on nonlocal usage with closures

--- utils/display.py
+++ utils/display.py
@@ -1,6 +1,9 @@
 def color_text_with(color, text=""):
   c_normal = "\033[0;0m"
   def color_text(text):
+    # for inmmutable variables () from the outer scope that we
+    # want to modify (like a counter variable), we would have 
+    # to define them as nonlocal here in the inner function scope
     return f'{color}{text}{c_normal}'
   if text:
     return color_text(text)
